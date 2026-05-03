14078e46564b0538bc1cdc986417628a27ad6219
t-stelma <t.stelmaszczyk1@gmail.com>
2026-05-03 12:27:44 +0200

    Fetch HICP data for all MVP countries

---
+++ data_pipeline/test_fetch_all_hicp.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+from extract.fetch_eurostat import fetch_dataset
+from transform.parse_eurostat import parse_single_geo_time_series
+from utils.config_loader import load_yaml_config
+
+
+countries_config = load_yaml_config("countries.yml")
+countries = countries_config["countries"]
+
+dataset_code = "prc_hicp_midx"
+
+all_country_frames = []
+
+for country in countries:
+    country_code = country["code"]
+
+    print(f"Fetching HICP data for {country_code}...")
+
+    params = {
+        "geo": country_code,
+        "coicop": "CP00",
+        "unit": "I15",
+        "lang": "en",
+    }
+
+    data = fetch_dataset(dataset_code, params=params)
+
+    df = parse_single_geo_time_series(
+        data=data,
+        dataset_code=dataset_code,
+        indicator_name="hicp_index",
+        country_code=country_code,
+    )
+
+    all_country_frames.append(df)
+
+combined_df = pd.concat(all_country_frames, ignore_index=True)
+
+print("\nCombined data preview:")
+print(combined_df.head())
+
+print("\nCountries loaded:")
+print(combined_df["country_code"].unique())
+
+print("\nRows per country:")
+print(combined_df.groupby("country_code").size())
+
+print("\nLatest rows per country:")
+print(combined_df.sort_values("time_period").groupby("country_code").tail(1))
